1a4ccb886761757c4de27e78502825b9194ab9d3
David Wells <wellsland@gmail.com>
2014-01-31 15:20:22 -0800

    Initial set-up of Main.m
    Setting up some basic framework for the project, similar to how the C++
    code was set up.

--- TennisInObjectiveC/main.m
+++ TennisInObjectiveC/main.m
@@ -20,14 +20,37 @@ int main(int argc, char * argv[])
 {
     @autoreleasepool {
         
+        // Set up variables and initialize them to some test values
+        int randSeed = 1000;
+        int prob1 = 70;
+        int prob2 = 70;
         
-        srandom(19);
-        Player *player1 = [[Player alloc] initWithProbability: 50];
-        Player *player2 = [[Player alloc] initWithProbability: 70];
+        // Reandom number generator with seed (default 1000)
+        srandom(randSeed);
+        
+        // Create and initialize 2 players with their respective probabilities
+        Player *player1 = [[Player alloc] initWithProbability: prob1];
+        Player *player2 = [[Player alloc] initWithProbability: prob2];
+        
+        // Create a MATCH - A match is the overall entity that encompases sets and games
+        
+        
+        // Create a score - and start playing the match!
+        // The result of the match goes into score
+        
+        
+        
+        // Print the final score for this tennis match
+        
+        
         Game *game = [[Game alloc] initWithFirstPlayer:player1 secondPlayer:player2];
         Score *gScore = [game play: player1];  // let player1 serve for this game.
         NSLog(@"%@", gScore);
         
+        
+        // All went well, so return!
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+        
+        
     }
 }
